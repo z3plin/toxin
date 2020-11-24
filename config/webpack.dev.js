d2161c19622db1bc907d6da8676faf777ecb0503
Serge <serj.raguzin@yandex.ru>
2020-11-24 15:08:45 +0300

    add live reload for pug-files

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
 const paths = require('./paths')
+const path = require('path')
 
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
@@ -11,11 +12,17 @@ module.exports = merge(common, {
 
     devServer: {
         historyApiFallback: true,
-        contentBase: paths.build,
+        // contentBase: paths.build,
+        contentBase: path.resolve(__dirname, '../src'),
+        watchContentBase: true,
+        overlay: {
+            warnings: true,
+            errors: true
+        },
         open: true,
         compress: true,
         hot: true,
-        port: 9000
+        port: 9000,
     },
 
     plugins: [
